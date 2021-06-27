6bbcc0ee44868a5dab39e30428d71a7039fc7b99
MannJaiswal <mannjaiswal5@gmail.com>
2021-06-27 14:57:40 -0700

    formatted the code to look much more prettier

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,37 @@
-let pokemonList = [{name: "Pikachu", 
-                    height: 15, 
-                    type: 'electric', 
-                    abilities: ["static", "lightninggrod"]
-                },
+let pokemonList = [
+{
+    name: "Pikachu", 
+    height: 15, 
+    type: 'electric', 
+    abilities: ["static", "lightninggrod"]
+},
 
-                {
-                    name: "Butterfree", 
-                    height: 43, 
-                    type: ['bug', 'flying'], 
-                    abilities: ["compoundeyes", "tinted-lens"]
-                },
+{
+    name: "Butterfree", 
+    height: 43, 
+    type: ['bug', 'flying'], 
+    abilities: ["compoundeyes", "tinted-lens"]
+},
 
-                {
-                    name: "Nidoking", 
-                    height: 55, 
-                    type: ['ground','poison'], 
-                    abilities: ["poison-point", "rivalry"]
-                },
+{
+    name: "Nidoking", 
+    height: 55, 
+    type: ['ground','poison'], 
+    abilities: ["poison-point", "rivalry"]
+},
 
-                {
-                    name: "Keldeo", 
-                    height: 50, 
-                    type: ['water','fighting'], 
-                    abilities: "justified"
-                },
+{
+    name: "Keldeo", 
+    height: 50, 
+    type: ['water','fighting'], 
+    abilities: "justified"
+},
 
-                {
-                    name: "Lapras", 
-                    height: 98, 
-                    type: ['water','ice'], 
-                    abilities: ["hydration","shell-armor"]
-                }];
+{
+    name: "Lapras", 
+    height: 98, 
+    type: ['water','ice'], 
+    abilities: ["hydration","shell-armor"]
+}];
                    
                 
