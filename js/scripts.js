02a5310b9f811cdbd5b89a18141c32de02d54327
MannJaiswal <mannjaiswal5@gmail.com>
2021-06-27 23:00:47 -0700

    Added styling to index.html to render all the pokemon list from the script.js file attached

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Creating a list of pokemon
 let pokemonList = [
 {
     name: "Pikachu", 
@@ -34,9 +35,21 @@ let pokemonList = [
     abilities: ["Hydration","Shell-Armor"]
 }];
 
-for (i =0; i < pokemonList.length; i++){
-    document.write("|Name: " + pokemonList[i].name + " |Height: " + pokemonList[i].height+ " |Type:" + pokemonList[i].type + " </br>");
-}
+
+// for loop to iterate over pokemon's list
+for (let i =0; i < pokemonList.length; i++){
+    document.write("|Name: " + pokemonList[i].name + " |Height: " + pokemonList[i].height+ " |Type: " + pokemonList[i].type + " |Abilities: "+ pokemonList[i].abilities +"|</br>");
+    if (pokemonList[i].height > 7){
+        document.write("|" +pokemonList[i].name+ " is a huge pokemon!" + "|</br>");
+    }
+        else if (pokemonList[i].height > 4 && pokemonList[i].height < 6.9) {
+            document.write("|"+pokemonList[i].name + " is average in its size" + "|</br>");
+        }
+        else {
+            document.write("|"+pokemonList[i].name+" is small but powerful" + "|</br>");
+        }
+    }
+
 
                    
                 
