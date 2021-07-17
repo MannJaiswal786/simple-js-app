7c54b75d7ea2c64cf6c92f1c75c5f1c31f61c299
MannJaiswal <mannjaiswal5@gmail.com>
2021-07-16 20:42:46 -0700

    Added the to top button using html, css and javascript

--- js/scripts.js
+++ js/scripts.js
@@ -152,3 +152,26 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+//------------------------------------------------------------------------------------------------------------------------------------------------//
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
