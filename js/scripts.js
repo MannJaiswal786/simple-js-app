89df354fde7ec10e0d227f8f3b02420e068fb5d1
MannJaiswal <mannjaiswal5@gmail.com>
2021-07-17 18:40:50 -0700

    removed the unnecessary comments

--- js/scripts.js
+++ js/scripts.js
@@ -153,7 +153,6 @@ pokemonRepository.loadList().then(function () {
   });
 });
 
-//------------------------------------------------------------------------------------------------------------------------------------------------//
 //Get the button
 var mybutton = document.getElementById("myBtn");
 
